fe347764249c2b34e8c5e8945a1d2bf3a58c6425
hatuko <hatsu.sherry@gmail.com>
2024-11-10 20:41:05 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+</head>
+
+<body>
+
+  <P>
+    <?php
+    $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($personal_data);
+    ?>
+
+  </P>
+</body>
+
+</html>
